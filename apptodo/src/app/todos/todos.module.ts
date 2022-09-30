7ec29ea1541e461370cf8a605ddf81499d69b212
garus_ae <garus.ae@weldbook.ru>
2022-09-30 22:01:55 +0300

    init:start project

---
+++ apptodo/src/app/todos/todos.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+
+import { TodosComponent } from 'src/app/todos/components/todos/todos.component';
+
+
+@NgModule({
+  declarations: [TodosComponent],
+  imports: [],
+})
+export class TodosModule {
+
+}
