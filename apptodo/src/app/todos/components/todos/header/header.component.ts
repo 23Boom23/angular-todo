f2e12669f5b55c71d19e53a75c480a0c645dc1af
garus_ae <garus.ae@weldbook.ru>
2022-09-30 22:48:55 +0300

    feat:add header and services

---
+++ apptodo/src/app/todos/components/todos/header/header.component.ts
@@ -0,0 +1,31 @@
+import { TodosService } from './../service/todos.service';
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-todos-header",
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+
+  text: string = '';
+
+  constructor(private todosService: TodosService) {
+    this.todosService.todos$.subscribe((todos) => {
+      console.log(todos);
+    })
+  }
+
+  changeText(event: Event):void {
+    const target = event.target as HTMLInputElement;
+    this.text = target.value;
+    console.log(target.value);
+  }
+
+  addTodo(): void {
+    console.log('addTOdo', this.text)
+    this.todosService.addTodo(this.text)
+    this.text=''
+  }
+
+
+}
--- apptodo/src/app/todos/todos.module.ts
+++ apptodo/src/app/todos/todos.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 import { TodosComponent } from 'src/app/todos/components/todos/todos.component';
+import { HeaderComponent } from "src/app/todos/components/todos/header/header.component";
+import { TodosService } from "./components/todos/service/todos.service";
+import { MainComponent } from "./components/todos/main/main.component";
+
+const routes: Routes = [
+  {path:'', component: TodosComponent}
+]
 
 
 @NgModule({
-  declarations: [TodosComponent],
-  imports: [],
+  declarations: [TodosComponent, HeaderComponent, MainComponent],
+  imports: [RouterModule.forChild(routes)],
+  providers: [TodosService],
 })
 export class TodosModule {
 
